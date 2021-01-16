83c0891d6f3d79d76f20d4bf2324e1219da38416
rawn01 <rohan.kaos@gmail.com>
2021-01-16 23:19:02 +0530

    working code

---
+++ src/actions/action.js
@@ -0,0 +1,9 @@
+const increment = () => ({
+    type: "increment"
+});
+
+const decrement = () => ({
+    type: "decrement"
+});
+
+export { increment, decrement };
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {increment, decrement } from "../actions/action";
 
 function App() {
+  const store = useSelector((store) => store);
+  const dispatch = useDispatch();
+
+  const inc = () => {
+    dispatch(increment());
+  }
+
+  const dec = () => {
+    dispatch(decrement());
+  }
 
   return (
     <div id='main'>
-      
+      <h3 data-testid='counter'>Counter: {store.count}</h3>
+      <button onClick={inc}>+</button>
+      <button onClick={dec}>-</button>
     </div>
   );
 }
